90384e06e4336d6c86931a8ba1806872838f4d0c
Tim Pease <tim.pease@gmail.com>
2009-06-17 16:47:42 -0600

    adding lots of documentation to the server clas

--- lib/servolux/server.rb
+++ lib/servolux/server.rb
@@ -1,17 +1,120 @@
 
+# == Synopsis
 # The Server class makes it simple to create a server-type application in
 # Ruby. A server in this context is any process that should run for a long
-# period of time either in the foreground or as a daemon process.
+# period of time either in the foreground or as a daemon.
 #
-# Use by inheritance.
+# == Details
+# The Server class provides for standard server features: process ID file
+# management, signal handling, run loop, logging, etc. All that you need to
+# provide is a +run+ method that will be called by the server's run loop.
+# Optionally, you can provide a block to the +new+ method and it will be
+# called within the run loop instead of a run method.
 #
-# Use by extending.
+# SIGINT and SIGTERM are handled by default. These signals will gracefully
+# shutdown the server by calling the +shutdown+ method (provided by default,
+# too). A few other signals can be handled by defining a few methods on your
+# server instance. For example, SIGINT is hanlded by the +int+ method (an
+# alias for +shutdown+). Likewise, SIGTERM is handled by the +term+ method
+# (another alias for +shutdown+). The following signal methods can be
+# supplied:
 #
-# Use by block/proc.
+#    Method  |  Signal  |  Default Action
+#    --------+----------+----------------
+#    hup        SIGHUP     none
+#    int        SIGINT     shutdown
+#    term       SIGTERM    shutdown
+#    usr1       SIGUSR1    none
+#    usr2       SIGUSR2    none
 #
-# Use by class << self.
+# In order to handle SIGUSR1 you would define a +usr1+ method for your
+# server.
 #
-# Handling signals.
+# There are a few other methods that are useful and should be mentioned. Two
+# methods are called before and after the run loop starts: +before_starting+
+# and +after_starting+. The first is called just before the run loop thread
+# is created and started. The second is called just after the run loop
+# thread has been created (no guarantee is made that the run loop thread has
+# actually been scheduled).
+#
+# Likewise, two other methods are called before and after the run loop is
+# shutdown: +before_stopping+ and +after_stopping+. The first is called just
+# before the run loop thread is signaled for shutdown. The second is called
+# just after the run loop thread has died; the +after_stopping+ method is
+# guarnteed to NOT be called till after the run loop thread is well and
+# truly dead.
+# 
+# == Usage
+# For simple, quick and dirty servers just pass a block to the Server
+# initializer. This block will be used as the run method.
+#
+#    server = Servolux::Server.new('Basic', :interval => 1) {
+#      puts "I'm alive and well @ #{Time.now}"
+#    }
+#    server.startup
+#
+# For more complex services you will need to define your own server methods:
+# the +run+ method, signal handlers, and before/after methods. Any pattern
+# that Ruby provides for defining methods on objects can be used to define
+# these methods. In a nutshell:
+#
+# Inheritance
+#
+#    class MyServer < Servolux::Server
+#      def run
+#        puts "I'm alive and well @ #{Time.now}"
+#      end
+#    end
+#    server = MyServer.new('MyServer', :interval => 1)
+#    server.startup
+#
+# Extension
+#
+#    module MyServer
+#      def run
+#        puts "I'm alive and well @ #{Time.now}"
+#      end
+#    end
+#    server = Servolux::Server.new('Module', :interval => 1)
+#    server.extend MyServer
+#    server.startup
+#
+# Singleton Class
+#
+#    server = Servolux::Server.new('Singleton', :interval => 1)
+#    class << server
+#      def run
+#        puts "I'm alive and well @ #{Time.now}"
+#      end
+#    end
+#    server.startup
+#
+# == Examples
+#
+# === Signals
+# This example shows how to change the log level of the server when SIGUSR1
+# is sent to the process. The log level toggles between "debug" and the
+# original log level each time SIGUSR1 is sent to the server process. Since
+# this is a module, it can be used with any Servolux::Server instance.
+#
+#    module DebugSignal
+#      def usr1
+#        if @old_log_level
+#          logger.level = @old_log_level
+#          @old_log_level = nil
+#        else
+#          @old_log_level = logger.level
+#          logger.level = :debug
+#        end
+#      end
+#    end
+#
+#    server = Servolux::Server.new('Debugger', :interval => 2) {
+#      logger.info "Running @ #{Time.now}"
+#      logger.debug "hey look - a debug message"
+#    }
+#    server.extend DebugSignal
+#    server.startup
 #
 class Servolux::Server
   include ::Servolux::Threaded
