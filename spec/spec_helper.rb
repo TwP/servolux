1147ac52b1bd443ee99882f17544fa967e952086
Tim Pease <tim.pease@gmail.com>
2012-01-06 17:34:16 -0700

    removing unneeded comments

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,15 +14,6 @@ include Logging.globally
 RSpec.configure do |config|
   include RSpec::LoggingHelper
   config.capture_log_messages
-
-  # == Mock Framework
-  #
-  # RSpec uses it's own mocking framework by default. If you prefer to
-  # use mocha, flexmock or RR, uncomment the appropriate line:
-  #
-  # config.mock_with :mocha
-  # config.mock_with :flexmock
-  # config.mock_with :rr
 end
 
 end  # unless defined?
