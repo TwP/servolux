4abdbdb56d0e321a3f33f8c5a82e241aee6cf796
Tim Pease <tim.pease@gmail.com>
2012-02-07 11:17:54 -0700

    One more try for figuring out this signal timing.
    I've increased the wait period ever so slightly, but I'm not hopeful. Last try
    before giving up on making Travis-CI happy.

--- spec/server_spec.rb
+++ spec/server_spec.rb
@@ -5,7 +5,7 @@ describe Servolux::Server do
 
   def wait_until( seconds = 5 )
     start = Time.now
-    sleep 0.1 until (Time.now - start) > seconds or yield
+    sleep 0.250 until (Time.now - start) > seconds or yield
   end
 
   base = Class.new(Servolux::Server) do
